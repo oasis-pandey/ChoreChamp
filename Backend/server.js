c025c180afb4c8da435669f0e930908de47eb428
Oasis Pandey <oasispandey2024@gmail.com>
2025-07-26 15:36:54 -0500

    add 5000 as alternative port

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import groupRoutes from "./routes/groupRoutes.js";
 import choreRoutes from "./routes/choreRoutes.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const MONGO_URL = process.env.MONGO_URL;
 
